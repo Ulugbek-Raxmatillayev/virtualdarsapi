09e78b79241536b2c4ec50514de210a5d3eec1cf
Ulugbek-Raxmatillayev <ulugbekraxmatillayev615@gmail.com>
2025-08-22 23:14:21 +0500

    updated

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const winston = require('winston')
 require('./startup/logging')()
 require('./startup/routes')(app);
 require('./startup/db')();
-
+require('./startup/prod')(app)
 const port = process.env.PORT || 5000;
 
 const server = app.listen(port, () => {
