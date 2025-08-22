4156d37da471ebdb618dfff5626f7395628412ed
Ulugbek-Raxmatillayev <ulugbekraxmatillayev615@gmail.com>
2025-08-23 01:02:45 +0500

    uodated

--- startup/logging.js
+++ startup/logging.js
@@ -1,10 +1,11 @@
 const winston = require('winston')
 require('winston-mongodb')
+const config = require('config')
 
 module.exports = function () {
     winston.add(new winston.transports.Console())
     winston.add(new winston.transports.File({ filename: 'logs/vd-logs.log', level: 'silly' }))
-    winston.add(new winston.transports.MongoDB({ db: 'mongodb://localhost/virtualdars-logs' }))
+    winston.add(new winston.transports.MongoDB({ db: config.get('db') }))
 
     winston.exceptions.handle(new winston.transports.Console(),new winston.transports.File({ filename: 'logs/vd-logs.log', level: 'silly' }))
 
